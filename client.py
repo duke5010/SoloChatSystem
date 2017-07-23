41a89f66c7add495a144faa5c30cf102024fc2ef
duke5010 <muthuvanoorsharon@gmail.com>
2017-07-23 13:51:19 -0400

    Add files via upload

---
+++ client.py
@@ -0,0 +1,34 @@
+import socket, threading
+import getpass
+
+def sendrecv():
+    while True:
+        data = sock.recv(1024)
+        if data is not '':
+            print data
+        else:
+            pass
+
+def Main():
+    global sock
+    sock = socket.socket()
+    HOST = '127.0.0.1'
+    PORT = 8080
+    sock.connect((HOST, PORT))
+    print ' [+] Connected to server!'
+    username = raw_input(' Please Enter your Username: ')
+    sock.send(username)
+    password =raw_input(' >> ')#getpass.getpass( ' Enter your password: ')
+    sock.send(password)
+    print ' [+] Authenticating...' 
+    if sock.recv(1) == 'S':
+        print ' [+] Authentication Sucess!'
+    elif sock.recv(1) == 'F':
+        quit(' [-] Error!')
+    while True:
+        thread = threading.Thread(target=sendrecv)
+        thread.start()
+        msg = raw_input('msg >> ')
+        sock.send(msg)
+if __name__ == '__main__':
+    Main()
